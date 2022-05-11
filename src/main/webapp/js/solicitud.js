2f98499c3b3c5b5f1428e6c82b937aa24261d63b
Kenie4r <54483710+Kenie4r@users.noreply.github.com>
2022-05-11 17:05:55 -0600

    Update solicitud.js

--- src/main/webapp/js/solicitud.js
+++ src/main/webapp/js/solicitud.js
@@ -31,22 +31,15 @@ dragArea.addEventListener("drop", (e)=>{
 })
 
 input.addEventListener("change", (e)=>{
-    alert(input.value);
-    /*let file = e.dataTransfer.files[0];
+    let file = e.target.files[0];
     let fileT = file.type;
     console.log(file)
     if(fileT =="application/pdf"){
         let text = document.getElementById("nameFile");
         text.innerHTML = file.name;
-        let reader = new FileReader();
-        reader.onload = ()=>{
-            let fileURL = reader.result;
-            input.value = fileURL;
-        }
-        reader.readAsDataURL(file)
     }else{
         alert("El tipo de archivo no es valido")
-    }*/
+    }
 })
 /*
 function filechange(event){
